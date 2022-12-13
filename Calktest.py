351316019d93b98e8bf7c74af4f68d0cd7bed303
debikde <percent000111@gmail.com>
2022-12-13 22:13:20 +0300

    Initial commit

---
+++ Calktest.py
@@ -0,0 +1,19 @@
+import unittest
+from laba5 import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+  def setUp(self):
+    self.calculator = Calculator()
+
+  def test_add(self):
+    self.assertEqual(self.calculator.summ(5,5), 10, "ошибка в сумме")
+  def test_subtract(self):
+    self.assertEqual(self.calculator.minus(10,3), 7, "ошибка в вычитании")
+  def test_multiply(self):
+    self.assertEqual(self.calculator.multiply(0,7), 0, "ошибка в умножении")
+  def test_divide(self):
+    self.assertEqual(self.calculator.divide(100,2), 50, "ошибка в делении")
+
+if __name__ == "__main__":
+  unittest.main()
---
+++ laba5.py
@@ -0,0 +1,41 @@
+class Calculator:
+  def __init__(self):
+    pass
+
+  def summ(self, x1, x2):
+    return x1 + x2
+
+  def multiply(self, x1, x2):
+    return x1 * x2
+
+
+  def minus(self, x1, x2):
+    return x1 - x2
+
+  def divide(self, x1, x2):
+    if x2 != 0:
+      return x1/x2
+    else:
+      return "error: x = 0"
+
+
+
+print("Введите число")
+x = int(input())
+print("Введите действие")
+comand = input()
+print("Введите второе число")
+x2 = int(input())
+calc = Calculator()
+if comand == "+":
+  print("ответ=", calc.summ(x,x2))
+
+if comand == "-":
+  print("ответ=",calc.minus(x,x2))
+
+if comand == "*":
+  print("ответ=",calc.multiply(x,x2))
+
+if comand == "/":
+  print("ответ=",calc.divide(x,x2))
+
